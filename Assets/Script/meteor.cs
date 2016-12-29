59db7d3fa63722da36ad5ffdb510dab208d556fe
Jutruc <julie.heugue@gmail.com>
2016-12-29 19:10:44 +0100

    Base
    La base

---
+++ Assets/Script/meteor.cs
@@ -0,0 +1,26 @@
+﻿    using UnityEngine;
+using System.Collections;
+
+public class meteor : MonoBehaviour {
+    public Transform cratere;
+    public int truc = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.name == "terrain") ;
+        {
+            Instantiate(cratere, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
